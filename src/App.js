4fe2ea0956fb664b44928c0175076c760dbcc31d
shivang yadav <shivang1691401@gmail.com>
2020-10-06 16:34:00 +0530

    done

--- src/App.js
+++ src/App.js
@@ -5,21 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div><h1>Shivang Yadav</h1></div>
+     <div>
+        <img src = {'https://www.freepnglogos.com/uploads/google-favicon-logo-20.png'} className="logo" height= {230} width={230} />
+        <img src = {'https://www.freepnglogos.com/uploads/microsoft-window-logo-emblem-0.png'} className="logo" height= {230} width={230} />
+      </div>
+      <div><p>They Both are Competitors</p></div>
     </div>
+     
+
   );
 }
 
